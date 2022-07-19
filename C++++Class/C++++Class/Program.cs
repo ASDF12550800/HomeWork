5b04629244c3af6641e9e049ede5774016c92e36
ASDF12550800 <109446750+ASDF12550800@users.noreply.github.com>
2022-07-19 12:22:43 +0900

    HomeWork_01

---
+++ C++++Class/C++++Class/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace C____Class {
+    class Program { 
+        static void Main(string[] args) {
+            Console.Write("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
