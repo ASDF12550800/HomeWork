77370dabaf8a9983480ded0732646857af7ee773
ASDF12550800 <109446750+ASDF12550800@users.noreply.github.com>
2022-07-20 14:38:40 +0900

    New HomeWork

--- C++++Class/C++++Class/Program.cs
+++ C++++Class/C++++Class/Program.cs
@@ -3,8 +3,27 @@
 namespace C____Class {
     class Program { 
         static void Main(string[] args) {
-            Console.Write("Hello World");
-            Console.ReadKey();
+            
+            // 01
+            {
+                Console.Write("Hello World");
+                Console.ReadKey();
+            }
+            
+            Console.Clear();
+
+            // 02
+            {
+                string input_1 = "", input_2 = "", str = "the string is : \n";
+                // ----------------------------------- //
+                Console.Write("input s1 : "); input_1 = Console.ReadLine();
+                Console.Write("input s2 : "); input_2 = Console.ReadLine(); 
+                // ----------------------------------- //
+                Console.WriteLine(str + $"{input_1}, {input_2}");
+                Console.WriteLine(str + "{0}, {1}", input_1, input_2);
+                Console.WriteLine(str + input_1 + ", " + input_2);
+                Console.WriteLine(new string(str + input_1 + ", " + input_2));
+            }
         }
     }
 }
