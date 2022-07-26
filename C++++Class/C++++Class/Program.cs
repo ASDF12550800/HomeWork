e451e49eede88a702e33ba31ceb18ae064306ae7
ASDF12550800 <109446750+ASDF12550800@users.noreply.github.com>
2022-07-26 22:04:55 +0900

    new

--- C++++Class/C++++Class/Program.cs
+++ C++++Class/C++++Class/Program.cs
@@ -1,60 +1,36 @@
 ﻿using System;
 
 namespace C____Class {
-    class Program { 
+    class Program {
         static void Main(string[] args) {
-            
-            // 01
+            Console.Write("Input : "); int Input_1 = Convert.ToInt32(Console.ReadLine());
+            string All_Culc = "";
+            All_Culc = "" + Input_1;
+            for (; ; )
             {
-                Console.Write("Hello World");
-                Console.ReadKey();
+                Console.Write("Type : "); string Type = Console.ReadLine();
+                Console.Write("Input : "); int Input_2 = Convert.ToInt32(Console.ReadLine());
+                All_Culc = "(" + All_Culc + Type + Input_2 + ")";
+                switch (Type)
+                {
+                    case "+": Input_1 = Input_1 + Input_2; break;
+                    case "-" : Input_1 = Input_1 - Input_2; break;
+                    case "*" : Input_1 = Input_1 * Input_2; break;
+                    case "/" : Input_1 = Input_1 / Input_2; break;
+                    default: break;
+                }
+                Console.WriteLine("Result : " + All_Culc + "=" + Input_1 + "\n\n");
+                Console.Write("Continue? (Y/N) : ");
+                string Answer = Console.ReadLine().ToLower();
+                if (Answer == "y") continue;
+                else if (Answer == "n") break;
+                else {
+                    Console.Write("error");
+                    return;
+                }
             }
-            
-            Console.Clear();
-
-            // 02
-            {
-                string input_1 = "", input_2 = "", str = "the string is : \n";
-                // ----------------------------------- //
-                Console.Write("input s1 : "); input_1 = Console.ReadLine();
-                Console.Write("input s2 : "); input_2 = Console.ReadLine(); 
-                // ----------------------------------- //
-                Console.WriteLine(str + $"{input_1}, {input_2}");
-                Console.WriteLine(str + "{0}, {1}", input_1, input_2);
-                Console.WriteLine(str + input_1 + ", " + input_2);
-                Console.WriteLine(new string(str + input_1 + ", " + input_2));
-                Console.ReadKey();
-            }
-
-            Console.Clear();
-
-            // 03
-            {
-                Console.Write("Number : ");
-                int    Int   = Convert.ToInt32(Console.ReadLine());
-                float  Float = Int;
-                string Str   = Convert.ToString(Float);
-                // --------------------------- //
-                Console.WriteLine("Int    : " + Int   + " | Type : " + Int.GetType());
-                Console.WriteLine("Float  : " + Float + " | Type : " + Float.GetType());
-                Console.WriteLine("String : " + Str   + " | Type : " + Str.GetType());
-                Console.ReadKey();
-            }
-
-            Console.Clear();
-
-            // 04
-            {
-                Console.Write("input float : "); float Input_1 = Convert.ToSingle(Console.ReadLine());
-                Console.Write("input float : "); float Input_2 = Convert.ToSingle(Console.ReadLine());
-                // ---------------------------------------------------- //
-                Console.WriteLine("+ : " + MathF.Round(Input_1 + Input_2, 2));
-                Console.WriteLine("% : " + (Input_1 % Input_2));
-                Console.WriteLine("- : " + (Input_1 - Input_2));
-                Console.WriteLine("/ : " + (Input_1 / Input_2));
-                Console.WriteLine("* : " + (Input_1 * Input_2));
-            }
-
+            Console.Write("\n\n<Finish>");
+            Console.ReadKey();
         }
     }
 }
